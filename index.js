96a0abd8653a32fd530666b0604385e7c79ea221
Michael Snowden <mtsnowden@gmail.com>
2019-06-06 20:07:39 -0500

    up and running

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
 
+const app = express();
 const port = process.env.PORT || 4001;
 
+app.use(express.static("public"))
+app.use(bodyParser.json())
+
 app.listen(port, () => {
- console.log(`Web server is listening on port ${port}!`);
+ console.log(`Your mom is listening on port ${port}!`);
 });
