3bca948e90cc31b88ae0975f75e253810cb1a2d4
Michael Snowden <mtsnowden@gmail.com>
2019-06-10 23:25:12 -0500

    commenting

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,13 +1,19 @@
 const comments = require("../data/comments");
 
+// return the json 
 const list = (req, res) => {
     return res.json(comments);
 }
 
+// search the comments and match the requested id with the one that 
+// matches in the json file
+// return that element
 const show = (req, res) => {
     const oneComment = comments.find(comment => comment._id == req.params.id)
-    return res.json(oneComment)}
+    return res.json(oneComment)
+}
 
+// 
 const create = (req, res) => {
     const addComment = req.body
     const counter = comments.length+1
