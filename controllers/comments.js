7761577c07618776a81ee40340322fb9f02252be
Michael Snowden <mtsnowden@gmail.com>
2019-06-11 09:56:36 -0500

    commenting

--- controllers/comments.js
+++ controllers/comments.js
@@ -13,7 +13,12 @@ const show = (req, res) => {
     return res.json(oneComment)
 }
 
-// 
+// create variable addComment which requests information from the body
+// counter keeps track of the length of the comments 
+// _id is the primary key on elements in a collection
+// we're setting the counter to the id count of each element in the json body
+// then pushing the new comment onto the comments array and 
+// returning that array
 const create = (req, res) => {
     const addComment = req.body
     const counter = comments.length+1
